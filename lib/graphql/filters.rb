19dc0ce5926c46de0588d2e84ed43d43f546ef60
Riccardo Agatea <riccardo@moku.io>
2023-02-02 18:00:36 +0100

    fix

--- lib/graphql/filters.rb
+++ lib/graphql/filters.rb
@@ -5,7 +5,7 @@ require_relative 'filters/version'
 require_relative 'filters/utility/cached_class'
 
 module Graphql
-  class Filters
+  module Filters
     # This will one day be a separate gem, and in that moment we will change this reference accordingly
     CachedClass = Utility::CachedClass
     private_constant :CachedClass
