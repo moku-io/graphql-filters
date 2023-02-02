32a41d1f1422f70c86c36ab30a768ff601180e67
Riccardo Agatea <riccardo@moku.io>
2023-02-02 16:28:02 +0100

    add cached class

--- lib/graphql/filters.rb
+++ lib/graphql/filters.rb
@@ -1,6 +1,10 @@
 require_relative 'filters/version'
+require_relative 'filters/utility/cached_class'
 
 module Graphql
   module Filters
+    # This will one day be a separate gem, and in that moment we will change this reference accordingly
+    CachedClass = Utility::CachedClass
+    private_constant :CachedClass
   end
 end
